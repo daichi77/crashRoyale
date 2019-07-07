c0388e5d316d538030bfba70592e54205eb5ddeb
daichi77 <b1738164@planet.kanazawa-it.ac.jp>
2019-07-07 19:22:21 +0900

    Initial Commit

---
+++ crashRoyale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crashRoyale
+//
+//  Created by 石田大智 on 2019/07/07.
+//  Copyright © 2019 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
